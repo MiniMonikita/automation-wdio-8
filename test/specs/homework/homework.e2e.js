9a2bfbf5be21d20e4e7ae194dd64432f049aaef3
Monika <m.kalajtzidu@gmail.com>
2023-04-26 17:38:50 +0200

    homework lesson1

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,8 +2,14 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
-        // sem vypracuj domácí úkol
+        await browser.reloadSession();
+        
+        await browser.url("registrace");
+
+        await browser.saveScreenshot("registrace_page.png")
+        
 
     });
 
 });
+
